0151e06b9621c7a81ad60524b12689f2b68ab4bd
Romserc3423 <leon45sergio@gmail.com>
2026-05-12 16:19:53 -0600

    Invertir cadena sin funcion

--- Primer-Parcial/Participacion/index.php
+++ Primer-Parcial/Participacion/index.php
@@ -1,20 +1,21 @@
 <?php
 
 // <!-- Ejemplo 4. Fibonacci -->
-function fibonacci($n){
+function fibonacci($n)
+{
 
 
-    $n1= 0;
-    $n2= 1;
-    $suma=0;
-    for ($i=0; $i < $n; $i++) { 
+    $n1 = 0;
+    $n2 = 1;
+    $suma = 0;
+    for ($i = 0; $i < $n; $i++) {
 
         $suma = $n1 + $n2;
-        $n1 = $n2; 
+        $n1 = $n2;
         $n2 = $suma;
-        echo $n1 ."</br>";
+        echo $n1 . "</br>";
     }
-   
+
 }
 echo fibonacci(10);
 
@@ -22,19 +23,18 @@ echo "<br>";
 
 // Ejemplo 5. Invertir una cadena de texto
 
-function invertirCadena($texto){
 
-        $array_text = str_split($texto);
-        
-        $last_Position = array_key_last($array_text);
+    $texto = "Perra ayil";
+    $array_text = str_split($texto);
 
-    for ($i=$last_Position; $i >= 0 ; $i--) { 
-    echo $array_text[$i];
+    $last_Position = array_key_last($array_text);
 
-           }
-}
+    for ($i = $last_Position; $i >= 0; $i--) {
+        echo $array_text[$i];
+    }
+  
 
-echo invertirCadena("Hola Mundo");
+
 
 
 
@@ -47,12 +47,15 @@ echo invertirCadena("Hola Mundo");
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Participacion 1</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
